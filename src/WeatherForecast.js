59af0b1f30d7ebeef9ec2ccda4f5436a16b1ef88
cpmssz <ccarla.pms@gmail.com>
2024-08-14 18:53:17 +0100

    Forecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,21 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+    let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+
+
   return (
     <div className="WeatherForecast">
       <div className="row">
