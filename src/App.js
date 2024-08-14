967c9cf1fd4a2c30df7fe597fa20837401f92f0e
cpmssz <ccarla.pms@gmail.com>
2024-08-14 14:16:23 +0100

    Display the weather of a city, by adding axios integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello fyf</h1>
+        <Weather city="Porto"/>
         <a
           className="App-link"
           href="https://reactjs.org"
